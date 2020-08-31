505b8f1523ce5d91ce5dcba3537027e3a82fac6d
Zoltan Jakubecz <jakuzo@gmail.com>
2020-08-31 08:37:12 +0200

    Initial commit

---
+++ carservices/src/main/java/com/jakuza/carservices/CarservicesApplication.java
@@ -0,0 +1,13 @@
+package com.jakuza.carservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarservicesApplication.class, args);
+	}
+
+}
---
+++ carservices/src/test/java/com/jakuza/carservices/CarservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakuza.carservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
