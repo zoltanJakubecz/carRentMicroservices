7160f23f59e532d9a71c0bfd645483ebaf24450f
Zoltan Jakubecz <jakuzo@gmail.com>
2020-09-03 21:57:30 +0200

    implement rent dto

--- rent-service/src/main/java/com/jakuza/rentservice/model/dto/RentalDto.java
+++ rent-service/src/main/java/com/jakuza/rentservice/model/dto/RentalDto.java
@@ -1,5 +1,24 @@
 package com.jakuza.rentservice.model.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class RentalDto {
+
+    private Long id;
+
+    private Long car_id;
+    
+    private LocalDateTime rentFrom;
+
+    private LocalDateTime rentTo;
 
 }
