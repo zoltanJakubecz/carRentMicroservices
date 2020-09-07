aeb5cd9fc3548ad81b87c5339481e01bf9e02f6b
Zoltan Jakubecz <jakuzo@gmail.com>
2020-09-07 17:01:07 +0200

    basic api gateway setup

--- api-gateway/src/main/java/com/jakuza/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/jakuza/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,15 @@ package com.jakuza.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
