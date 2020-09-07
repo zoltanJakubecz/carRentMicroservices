1e7e11a33f54ff55e7684ead37e67b2bec5318f1
Zoltan Jakubecz <jakuzo@gmail.com>
2020-09-07 15:03:34 +0200

    fix query

--- rent-service/src/main/java/com/jakuza/rentservice/repository/RentalRepository.java
+++ rent-service/src/main/java/com/jakuza/rentservice/repository/RentalRepository.java
@@ -3,10 +3,13 @@ package com.jakuza.rentservice.repository;
 import java.util.List;
 
 import com.jakuza.rentservice.model.Rental;
-import com.jakuza.rentservice.model.dto.RentalDto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-    public List<RentalDto> findAllByCarId(Long car_id);
+
+    @Query("SELECT r FROM Rental r WHERE r.car_id = :carId")
+    List<Rental> findByCar_id(@Param("carId") Long car_id);
 }
