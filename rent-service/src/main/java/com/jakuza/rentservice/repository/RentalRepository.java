8741f5f951d0c271ea69127e6ebb11a0dd559ab8
Zoltan Jakubecz <jakuzo@gmail.com>
2020-09-07 12:01:37 +0200

    implement car service

--- rent-service/src/main/java/com/jakuza/rentservice/repository/RentalRepository.java
+++ rent-service/src/main/java/com/jakuza/rentservice/repository/RentalRepository.java
@@ -1,9 +1,12 @@
 package com.jakuza.rentservice.repository;
 
+import java.util.List;
+
 import com.jakuza.rentservice.model.Rental;
+import com.jakuza.rentservice.model.dto.RentalDto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-    
+    public List<RentalDto> findAllByCarId(Long car_id);
 }
--- rent-service/src/main/java/com/jakuza/rentservice/service/RentService.java
+++ rent-service/src/main/java/com/jakuza/rentservice/service/RentService.java
@@ -3,14 +3,19 @@ package com.jakuza.rentservice.service;
 import java.util.List;
 
 import com.jakuza.rentservice.model.dto.RentalDto;
+import com.jakuza.rentservice.repository.RentalRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RentService {
 
+    @Autowired
+    private RentalRepository rentalRepository;
+
 	public List<RentalDto> getRentsForCar(Long car_id) {
-		return null;
+		return rentalRepository.findAllByCarId(car_id);
 	}
     
 }
