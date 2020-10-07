ded5a64eab8f5b8693b78612dd5534e9e40ab80b
Zoltan Jakubecz <jakuzo@gmail.com>
2020-10-07 18:07:48 +0200

    Wire in to Eureka

--- car-service-nodejs/app.js
+++ car-service-nodejs/app.js
@@ -11,8 +11,45 @@ app.use(express.json());
 
 app.use('/api/cars', carRoute);
 
+// ------------------ Eureka Config --------------------------------------------
+
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const eureka = new Eureka({
+  instance: {
+    app: 'carservice',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:8081',
+    port: {
+      '$': 8081,
+      '@enabled': 'true',
+    },
+    vipAddress: 'localhost',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'localhost',
+    port: 8761,
+    servicePath: '/eureka/apps/'
+  }
+});
+eureka.logger.level('debug');
+eureka.start(function(error){
+  console.log(error || 'complete');
+});
+
+
+// ------------------ Server Config --------------------------------------------
+
+
+
 const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
-})
+});
