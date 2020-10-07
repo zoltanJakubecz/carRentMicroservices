15ee40084886ee5e4d58a66f3d5fe361c440da12
Zoltan Jakubecz <jakuzo@gmail.com>
2020-10-07 16:42:58 +0200

    configure app

--- car-service-nodejs/app.js
+++ car-service-nodejs/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const PORT = 5000;
+const carRoute = require('./routes/CarRoute')
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/cars', carRoute);
+
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
