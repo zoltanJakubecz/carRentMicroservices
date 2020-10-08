59865ef3cc18420f82a164195df1b5f10f4e915a
Zoltan Jakubecz <jakuzo@gmail.com>
2020-10-08 08:19:19 +0200

    add find by id route

--- car-service-nodejs/routes/CarRoute.js
+++ car-service-nodejs/routes/CarRoute.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const { getCars } = require('../services/car.sevice');
 const service = require('../services/car.sevice')
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+
         const cars = await service.getCars();
         res.status(200).json(cars.rows);
-    
+
 });
+
+router.get('/:id', async (req, res) => {
+
+
+    const car = await service.find(req.params.id);
+    if(!car.rows[0]) res.status(404).send('Not found');
+    res.status(200).send(car.rows[0]);
+
+})
 
 
 module.exports = router;
--- car-service-nodejs/services/car.sevice.js
+++ car-service-nodejs/services/car.sevice.js
@@ -8,7 +8,14 @@ const getCars = () => {
         console.error(error.message);
     }
     
-} 
+};
+
+const find = (id) => {
+    return pool.query("SELECT * FROM car WHERE id = $1", [id]);
+}
 
 
-module.exports = {getCars};
+module.exports = {
+    getCars,
+    find
+};
