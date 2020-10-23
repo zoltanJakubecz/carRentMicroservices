969a57de9fe04a77dc961fb366361024536c8220
Zoltan Jakubecz <jakuzo@gmail.com>
2020-10-23 14:08:57 +0200

    remove java spring carservice

--- carservices/src/main/java/com/jakuza/carservices/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jakuza.carservices.repository;
-
-import com.jakuza.carservices.model.Car;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-    
-}
