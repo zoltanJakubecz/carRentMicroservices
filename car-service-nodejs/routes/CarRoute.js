edc3ec741a9da6bac360daec902c084390e597bf
Zoltan Jakubecz <jakuzo@gmail.com>
2020-10-23 22:45:24 +0200

    add error handling to api

--- car-service-nodejs/routes/CarRoute.js
+++ car-service-nodejs/routes/CarRoute.js
@@ -4,34 +4,53 @@ const service = require('../services/car.sevice')
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-
-        const cars = await service.getCars();
-        for(car of cars.rows){
-            car.image =  `http://localhost:8762/imgs/${car.image}`;
+        try {
+            const cars = await service.getCars();
+            for(car of cars.rows){
+                car.image =  `http://localhost:8762/imgs/${car.image}`;
+            }
+            res.status(200).json(cars.rows);            
+        } catch (error) {
+            console.log(error);
+            res.status(404);
         }
-        res.status(200).json(cars.rows);
 
 });
 
 router.get('/:id', async (req, res) => {
-
-    const car = await service.find(req.params.id);
-    if(!car.rows[0]) res.status(404).send('The given id was not found');
-    res.status(200).send(car.rows[0]);
+    try {
+        const car = await service.find(req.params.id);
+        if(!car.rows[0]) res.status(404).send('The given id was not found');
+        res.status(200).send(car.rows[0]);
+        
+    } catch (error) {
+        console.error(error);
+        res.status(404);        
+    }
 
 });
 
 
 router.post('/', async (req, res) => {
-    const newCar = await service.addCar(req.body);
-    res.send(newCar.rows[0]);
+    try {
+        const newCar = await service.addCar(req.body);
+        res.status(200).send(newCar.rows[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(404);
+    }
+    
 });
 
 router.put('/:id', async (req,res) => {
     const { id } = req.params;
-    const updateCar = await service.update(id, req.body);
-    console.log(updateCar);
-    res.send(updateCar.rows[0]);
+    try {
+        const updateCar = await service.update(id, req.body);
+        res.send(updateCar.rows[0]);
+    } catch (error) {
+        res.status(404);
+    }
+    
 })
 
 
