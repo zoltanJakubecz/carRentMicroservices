59a3453bfd5bc92988bb1d68750b667d512df8a7
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-25 20:34:19 +0100

    fix typo

--- car-service-nodejs/database/db.js
+++ car-service-nodejs/database/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.DB_USEF,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
